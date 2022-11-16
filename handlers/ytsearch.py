5fa3283a826c0f523a0c6b82d90c6ce1c281db8b
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2022-11-16 18:29:47 +0400

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -16,14 +16,14 @@ import pyrogram
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@app.on_message(pyrogram.filters.command(["ara"]))
+@app.on_message(pyrogram.filters.command(["search", "axtar"]))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("Mənə sənətçi adı ya da mahnı adı verin!")
+            await message.reply_text("ℹ Mənə Sənətçi Adı Ya Da Musiqi Adı Verin!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Axtarıram....")
+        m = await message.reply_text("🔎 🎧 AXTARILIR...")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
